cb94c1c6466b42c3b9f2fe6a5b72d1adb42e7820
DevEnv <hamzaali151214@hotmail.com>
2021-07-06 15:30:07 +0530

    cmake windows

---
+++ cmake_Stuff/cmake_1/CMakeFiles/cmake_1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmake_1.dir/main.cpp.o"
+  "cmake_1"
+  "cmake_1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cmake_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake_Stuff/cmake_2/CMakeFiles/cmake_2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmake_2.dir/main.cpp.o"
+  "cmake_2"
+  "cmake_2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cmake_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
