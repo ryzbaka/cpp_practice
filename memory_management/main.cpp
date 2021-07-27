02e1abcf6271abc6d5b16ef01e9d9404b7d11286
Hamza Ali <hamzaali151214@hotmail.com>
2021-07-27 11:20:02 +0530

    Rule of 3

--- memory_management/main.cpp
+++ memory_management/main.cpp
@@ -3,28 +3,53 @@
 /*
     Local variables go out of scope when the function ends.
     We use free store to make sure this doesn't happen when we don't want it to.
-    The 'new' keyword is used to obtain memory from the free store. It allocates memory, runs constructor and returns a pointer.
-    The 'delete' keyword is used to call the desctructor and free up memory.
+    The 'new' keyword is used to obtain memory from the free store. It allocates
+   memory, runs constructor and returns a pointer. The 'delete' keyword is used
+   to call the destructor and free up memory. The free store is for longer
+   lived variables.
 */
-class Resource
-{
+class Resource {
 private:
-    std::string name;
+  std::string name;
 
 public:
-    Resource(std::string n) : name(n){};
-    ~Resource() { std::cout << "Destroyed: " << this->name << "\n"; };
-    std::string getName() const { return name; };
+  Resource(std::string n) : name(n){};
+  ~Resource() { std::cout << "Destroyed: " << this->name << "\n"; };
+  std::string getName() const { return name; };
 };
-int main()
-{
-    {
-        Resource localResource = Resource("local");
-        std::string localString = localResource.getName();
-    }
+int main() {
+  {
+    Resource localResource =
+        Resource("local"); // local variable stored on the stack.
+    std::string localString = localResource.getName();
+  }
 
-    Resource *resource_ptr = new Resource("created with new");
-    std::string new_string = resource_ptr->getName();
-    delete resource_ptr;
-    return 0;
+  Resource *resource_ptr =
+      new Resource("created with new"); // objects created with the new keyword
+                                        // are stored in the free store (heap).
+  std::string new_string = resource_ptr->getName();
+  delete resource_ptr;
+  resource_ptr =
+      nullptr; // common practice of setting pointer to nullptr after deleting,
+               // since null pointer exceptions are easier to catch.
+  // RULE OF THREE
+  /*
+      If you're going to have a pointer to the free store (heap), keep it in an
+     object so that we can use member functions to manage it. For example, the
+     destructor of the object can `delete` the pointer so that memory gets
+     cleaned up automatically when the object goes out of scope. Another issue
+     that we can face when doin manual memory management is running into errors
+     when copying pointers. Copy constructors can be used to work our way around
+     such issues. The copy assignment operator not only initializes the new
+     object with the values of the old object whose copy you're making
+
+  */
+  // RULE OF FIVE
+  /*
+      In a scenario where an expensive resource is stored in an object that is
+     about to go out of scope, we can move the resource to another object rather
+     than deleting and creating again. We can do this using a move constructor
+     and a move assignment operator.
+  */
+  return 0;
 }
---
+++ memory_management/rule_of_three.cpp
@@ -0,0 +1,90 @@
+// RULE OF THREE
+/*
+    If you're going to have a pointer to the free store (heap), keep it in an
+   object so that we can use member functions to manage it. For example, the
+   destructor of the object can `delete` the pointer so that memory gets cleaned
+   up automatically when the object goes out of scope. Another issue that we can
+   face when doin manual memory management is running into errors when copying
+   pointers. Copy constructors can be used to work our way around such issues.
+   The copy assignment operator not only initializes the new object with the
+   values of the old object whose copy you're making
+
+    The object acts a "handle" for the free store pointer.
+    https://en.cppreference.com/w/cpp/language/rule_of_three
+
+    Our goal is to prevent memory leaks. A memory leak occurs when we lose our
+   ability to free up memory space because we have to lost access to the pointer
+   pointing to that memory space.
+
+   We use destructors to free up space. We use copy constructors to prevent the
+   calling of delete on a pointer that has already been deleted by creating a
+   fresh copy. We use copy assignment operators to prevent memory leakage by
+   loss of access to old pointer value before it could be deleted.
+*/
+
+#include <iostream>
+#include <string>
+using std::cout;
+using std::string;
+
+class Resource {
+private:
+  std::string name;
+
+public:
+  Resource(std::string n) : name(n) {
+    cout << "Constructing resource: " << n << "\n";
+  };
+  ~Resource() { std::cout << "Destroyed: " << this->name << "\n"; };
+  std::string getName() const { return name; };
+};
+
+class Person {
+private:
+  string firstName;
+  string lastName;
+  int age;
+  Resource *pResource;
+
+public:
+  Person(string first, string last, int num)
+      : firstName(first), lastName(last), age(num), pResource(nullptr) {}
+
+  Person(Person const &anotherPerson) // copy constructor
+      : firstName(anotherPerson.getFirst()), lastName(anotherPerson.getLast()),
+        age(anotherPerson.getAge()) {
+    pResource = new Resource(anotherPerson.pResource->getName());
+  }
+
+  ~Person() { delete pResource; }; // destructor
+  string getName() const { return firstName + " " + lastName; }
+  int getAge() const { return age; }
+  string getFirst() const { return firstName; }
+  string getLast() const { return lastName; }
+  void setFirstName(string first) {
+    cout << "changed first name to:\t" << first << "\n";
+    firstName = first;
+  }
+  void addResource() {
+    delete pResource; // free up memory at address that pResource is pointing
+                      // to. delete trigger the destructor.
+    pResource = new Resource("Resource for: " + this->getName());
+  }
+  Person &operator=(Person const &anotherPerson) { // copy assignment operator
+    delete pResource;
+    pResource = new Resource(anotherPerson.pResource->getName());
+    return *this;
+  }
+};
+
+int main() {
+  Person p1("Ethan", "Klein", 36);
+  p1.addResource();
+  p1.setFirstName("Hila");
+  p1.addResource();
+  Person p2 = p1; // copy constructor;
+  Person p3("Hamza", "Ali", 21);
+  p3.addResource();
+  p3 = p2;
+  return 0;
+}
